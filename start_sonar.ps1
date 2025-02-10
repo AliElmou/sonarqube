ad9734f9541609b27caa0176524456635ae71a4f
AliElmou <ali.elmoussaoui@polymtl.ca>
2025-02-10 13:57:06 -0500

    supprimer le script

--- start_sonar.ps1
+++
@@ -1,4 +0,0 @@
-cd sonar-application\build\distributions
-tar -xf sonar-application-25.2-SNAPSHOT.zip
-cd sonarqube-25.2-SNAPSHOT\bin\windows-x86-64
-Start-Process -NoNewWindow -FilePath .\StartSonar.bat
